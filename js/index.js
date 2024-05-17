268791164aa1b7d75028e677cdf6a1291d65cd1c
sieunhan12B <dgbaoo2104@gmail.com>
2024-05-17 17:28:08 +0700

    v2

--- js/index.js
+++ js/index.js
@@ -233,3 +233,13 @@ document.getElementById("btnHoaDon").onclick = () => {
   </tbody>
 </table>`;
 };
+
+document.addEventListener("keydown", function (event) {
+  if (event.ctrlKey) {
+    event.preventDefault();
+  }
+  if (event.keyCode == 123) {
+    event.preventDefault();
+  }
+});
+document.addEventListener("contextmenu", (event) => event.preventDefault());
